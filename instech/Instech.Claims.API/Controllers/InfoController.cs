114719179461ff95f5e7dedb5a6328044a486a32
Michael Voronov <michael.voronov@oceantg.com>
2022-03-25 15:58:37 +0100

    initial project

---
+++ instech/Instech.Claims.API/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Instech.Claims.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class InfoController : Controller
+    {
+        [HttpGet("")]
+        public string Index()
+        {
+            return "Instech Claim API";
+        }
+    }
+}
---
+++ instech/Tm.Instech.Data/InstClaim.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tm.Instech.Data
+{
+    public enum InstClaimType
+    {
+        Collision,
+        Grounding,
+        BadWeather,
+        Fire
+    }
+
+    public class InstClaim
+    {
+        public Guid Id { get; set; }
+        public int Year { get; set; }
+        public string? Name { get; set; }
+        public InstClaimType Type { get; set; }
+        public decimal DamageCost { get; set; }
+    }
+}
